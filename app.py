9c5635c30cef4756be98cfa7e8151417dc42750b
Varad <varadraut@gmail.com>
2022-04-30 23:09:02 -0600

    CORS support

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
+from flask_cors import CORS
 from models import vrp1
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -16,3 +18,6 @@ def solve(model):
     # return {
     #     'routes': [[[40.008992173966995, -105.28547778746469], [40.00320668011718, -105.24633899351937]], [[40.008992173966995, -105.28547778746469], [40.00320668011718, -105.24633899351937]]]
     # }
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
